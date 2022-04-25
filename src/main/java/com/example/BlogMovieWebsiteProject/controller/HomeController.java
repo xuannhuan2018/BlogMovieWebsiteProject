2e9e1bbb65a5a344b50d02577e3b0b0b1fbef643
18110282 <hieuho10022000@gmail.com>
2022-04-25 14:27:52 +0700

    Add interface of search input

--- src/main/java/com/example/BlogMovieWebsiteProject/controller/HomeController.java
+++ src/main/java/com/example/BlogMovieWebsiteProject/controller/HomeController.java
@@ -3,6 +3,8 @@ package com.example.BlogMovieWebsiteProject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -10,6 +12,12 @@ public class HomeController {
     @GetMapping("/")
     public String getIndex(){
         return "/views/index";
+    }
+
+    @GetMapping("/test/search")
+    @ResponseBody
+    public String testSearch(@RequestParam(value = "keyword", required = false) String keyword){
+        return keyword;
     }
 
 }
